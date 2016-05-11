f6a69172fde4dafb7faf5984e0eac296a4274a5a
Melanie27 <melaniemcganney@gmail.com>
2016-05-10 18:56:11 -0700

    Assignment 9 completed

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,14 +12,22 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    
+    return [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
         /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
+        NSRange cheeseRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+        NSString *removeCheese = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+        return removeCheese;
+        
     } else {
         /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
+        return cheeseName;
+        
     }
 
     /*
@@ -31,9 +39,17 @@
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
+        
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        NSMutableString *modifiableStatement = [NSMutableString stringWithString:@"cheese"];
+        
+        return [NSString stringWithFormat:@"1 %@", modifiableStatement];
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        NSMutableString *modifiableStatement = [NSMutableString stringWithString:@"cheese"];
+        [modifiableStatement appendString:@"s"];
+    
+        return [NSString stringWithFormat: @ "%ld %@", cheeseCount, modifiableStatement];
     }
     
     /*
