4b083e50ca7efd3bf1d37c9c89881a58f8f508e8
Melanie27 <melaniemcganney@gmail.com>
2016-05-19 13:29:38 -0700

    assignment 13

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -7,27 +7,32 @@
 //
 
 #import "StarTrekArrays.h"
-
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    NSArray  *trekArray = [characterString componentsSeparatedByString:@";"];
+    return trekArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    NSString *trekString = [characterArray componentsJoinedByString:@";"];
+    return trekString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    NSArray *sortedArray = [characterArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    return sortedArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    [characterArray filteredArrayUsingPredicate:containsWorf];
     return NO;
 }
 
 @end
+
