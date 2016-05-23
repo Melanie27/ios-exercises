5df1c3f5454369d1ae591ae18c2bcbefc34bc032
Melanie27 <melaniemcganney@gmail.com>
2016-05-23 10:55:08 -0700

    assignment 15 completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,51 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSInteger intValue = [number intValue];
+    intValue *= 2;
+    return @(intValue);
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+        if (number == otherNumber) {
+            return @[@(number)];
+        }
+    
+        NSInteger lowerNumber, higherNumber;
+        //test which number is lower and begin the array with that number
+        if (number > otherNumber) {
+            higherNumber = number;
+            lowerNumber = otherNumber;
+        } else {
+            higherNumber = otherNumber;
+            lowerNumber = number;
+        }
+    
+        //create empty mutable array that items can be added to
+        NSMutableArray *returnArray = [NSMutableArray array];
+    
+    for (NSInteger n = lowerNumber; n <= higherNumber; n++ ) {
+        //each time you loop through add the next number to the mutable array
+        [returnArray addObject:@(n)];
+    }
+
+    return returnArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSInteger lowestNumber = NSIntegerMax;
+    //loop through the arrayofNumbers to find the lowest number
+    for (NSNumber *number in arrayOfNumbers) {
+        //unwrap the integer value
+        NSInteger intValue = [number intValue];
+        
+        if(intValue < lowestNumber) {
+            lowestNumber = intValue;
+        }
+    }
+    return lowestNumber;
 }
 
 @end
